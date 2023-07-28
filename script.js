5741f38265cfa5d0500ccbeb1758f8c7d8592492
Santiago Ramirez <danielsantiagor45@gmail.com>
2023-07-28 13:51:12 -0500

    listo para jugar

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 const canvas = document.querySelector('#canvas');
+const btnRight = document.querySelector('#right');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnDown = document.querySelector('#down');
 const juego = canvas.getContext('2d');
+const spanlive = document.querySelector('#live')
+const spanTime = document.querySelector('#time')
+const spanRecord = document.querySelector('#record')
 
-window.addEventListener('load',setCanvaSize)
-window.addEventListener('resize',setCanvaSize)
-// window.addEventListener('resize',startGame)
-// function startGame(){
-//     const fraccion = 0.7
-  
-//     let canvaSize;
-    
-//     if(window.innerWidth > window.innerHeight){
-//         canvaSize = window.innerHeight * fraccion
-        
-//         canvas.setAttribute('width', (canvaSize))
-//         canvas.setAttribute('height', (canvaSize))
-       
-//     }
-//       else{
-//         canvaSize = window.innerWidth * fraccion
-//         canvas.setAttribute('width', canvaSize)
-//         canvas.setAttribute('height', canvaSize)
-//       }
-    
-//     const elementSize = canvaSize/10;
-//     console.log(elementSize)
-//     juego.textAlign = 'end'
-//     juego.font = elementSize + 'px Verdana'
-//     for(let i= 1 ; i <= 10; i++){
-//     juego.fillText(emojis['X'],elementSize*i,elementSize)
-//     }
-// }
+window.addEventListener('load',setCanvaSize);
+window.addEventListener('resize',cero);
+window.addEventListener('resize',setCanvaSize);
 
 let canvaSize;
 let elementSize;
+let playerPosition = {
+  x:undefined,
+  y:undefined,
+};
+
+let giftPlayer = {
+  x: undefined,
+  y: undefined,
+};
+
+let enemyBomb = [];
+let level = 0;
+let live = 3;
+let initialTime = Date.now()
+let intervalTime = setInterval(showTime,100);
+// let intervalTime; 
+let time;
+let record = Infinity;
+if(!localStorage.getItem('record')){
+  spanRecord.innerHTML = 0;
+  localStorage.setItem('record'),Infinity;
+};
+let endTime;
+
 
 function setCanvaSize(){
-  const fraccion = 0.7    
+  const fraccion = 0.8    
     if(window.innerWidth > window.innerHeight){
         canvaSize = window.innerHeight * fraccion
         
@@ -52,31 +57,177 @@ function setCanvaSize(){
        elementSize = canvaSize/10;
 
       startGame();
-}
-function startGame(){
+};
 
-  
-  // console.log(elementSize)
-  juego.textAlign = 'end'
-  juego.font = elementSize + 'px Verdana'
-  // for(let i= 1 ; i <= 10; i++){
-  // juego.fillText(emojis['X'],elementSize*i,elementSize)
-  // }
+function startGame() {
+  juego.textAlign = 'end';
+  juego.font = elementSize + 'px Verdana';
+ 
+  showLive();
+  map = maps[level];
 
-  
-  map = maps[2]
+  if(!map){return overgame()
+  };
   mapRows = map.trim().split('\n');
-  console.log(mapRows)
+ 
   mapRowsColums =mapRows.map(fila => fila.trim().split(''))
   let emoji;
+  let posX;
+  let posY;
+  enemyBomb =[];
+  juego.clearRect(0,0,canvaSize,canvaSize);
 
-  for (let fila = 1 ; fila <= 10; fila++){
-    for (let  columna = 1 ; columna <= 10; columna++){
+  mapRowsColums.forEach((fila, rowI) => {
+    fila.forEach((columna,colI) => {
+      emoji = emojis[columna];
+      posX = elementSize * (colI + 1)
+      posY = elementSize * (rowI + 1)
+      juego.fillText(emoji,posX,posY)
 
-      emoji = mapRowsColums[fila - 1][columna - 1]
-      
-      juego.fillText(emojis[emoji],elementSize*columna, elementSize*fila)
-    }
+      if (columna == 'O'){
+        if(!playerPosition.x && !playerPosition.y){
+          playerPosition.x = posX;
+          playerPosition.y = posY
+          };
+      } else if( columna == 'I'){
+        giftPlayer.x = posX;
+        giftPlayer.y = posY
+      } else if(columna == 'X'){
+        enemyBomb.push({x: posX, y:posY})
+      }
+    })
+    });
+  if (showRecord()) spanRecord.innerHTML = localStorage.getItem('record'); 
+  showTime();
+  movePlayer();
+    
+};
+function showLive(){
+ spanlive.innerHTML =  ''
+ let arrayLive =Array(live).fill(emojis['HEART'])
+ arrayLive.forEach(corazon=> {spanlive.append(corazon)})
+};
+function showTime(){
+  let timeNow = Date.now();
+  time = timeNow - initialTime;
+  spanTime.innerHTML = time;
+  
+ 
+};
+function showRecord(){
+  
+  if( endTime < localStorage.getItem('record')){
+    record = endTime
+    localStorage.setItem('record',record)
   }
+ 
+  spanRecord.innerHTML = localStorage.getItem('record'); 
+  
+};
+function winLevel(){
+  let giftX= playerPosition.x.toFixed(2) == giftPlayer.x.toFixed(2);
+  let gifty= playerPosition.y.toFixed(2) == giftPlayer.y.toFixed(2);
+  let giftConfirm = giftX && gifty;
+  console.log({playerPosition})
+  if(giftConfirm){
+    level++;
+    startGame();
+  }
+  
+};
+
+function overgame(){ console.log('ter')
+  clearInterval(intervalTime)
+  endTime = time
+  showRecord()
+};
+function choqueBomb(){
+  bombConfirm = enemyBomb.find(objetos=> {
+    let bombX= playerPosition.x.toFixed(1) == objetos.x.toFixed(1);
+    let bomby= playerPosition.y.toFixed(1) == objetos.y.toFixed(1);
+    return bombX && bomby;
+     
+  })
+  
+  if (bombConfirm){ 
+    playerPosition.x = undefined; 
+    playerPosition.y = undefined;
+    live--
+    if(live<=0){
+      level = 0;
+      live = 3;
+      intervalTime = Date.now()
+    };startGame()};
+  }
+function movePlayer(){
+  winLevel()
+  choqueBomb()
+  juego.fillText(emojis['PLAYER'], playerPosition.x , playerPosition.y);
+
+};
+  
+ 
+ 
+ 
+//Deteccion de teclas
+
+window.addEventListener('keydown',detectClicKeyBoard);
+btnUp.addEventListener('click',moveUp);
+btnRight.addEventListener('click',moveRight);
+btnLeft.addEventListener('click',moveLeft);
+btnDown.addEventListener('click',moveDown);
+
+function cero(){
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+}
+function detectClicKeyBoard(tecla){
+
+  switch (tecla.key) {
+    case 'ArrowUp':
+      moveUp()
+      break;
+    case 'ArrowDown':
+      moveDown()
+      break;
+    case 'ArrowRight':
+      moveRight()
+    break;
+    case 'ArrowLeft':
+      moveLeft()
+    break;  
+   
+  }
+}
+function moveUp(){
+  console.log({playerPosition,canvaSize})
+  if (playerPosition.y  > 1.5*elementSize){  playerPosition.y -= elementSize;
+  
+    startGame()}
 
 }
+function moveRight(){
+  if (playerPosition.x <= canvaSize - elementSize)
+  {playerPosition.x += elementSize;
+  startGame()}
+}
+function moveLeft(){
+  if (playerPosition.x >1.5* elementSize )
+  {playerPosition.x -= elementSize;
+  startGame()}
+}
+function moveDown(){
+  if ( playerPosition.y < canvaSize )
+  {playerPosition.y += elementSize;
+  startGame()}
+}
+
+
+
+
+
+
+
+
+
+
